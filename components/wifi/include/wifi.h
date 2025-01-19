4f4294f3e549b0fd30aebf1713f5a35e9b30eb6a
yhlee <skwieua@gmail.com>
2025-01-19 20:18:53 +0900

    add wifi component, can connect to ap

---
+++ components/wifi/include/wifi.h
@@ -0,0 +1,6 @@
+#ifndef _WIFI_H_
+#define _WIFI_H_
+
+void init_wifi(void);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,10 +1,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "led.h"
-// #include "wifi.h"
+#include "wifi.h"
 
 void app_main(void)
 {
     init_led();
-    turn_on_led();    
+    turn_on_led();
+
+    init_wifi();
 }
